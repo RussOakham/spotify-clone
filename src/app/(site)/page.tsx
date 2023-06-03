b51f04a2c311089154a2d0e037d0bfb6cf7bf3a2
RussOakham <73036915+RussOakham@users.noreply.github.com>
2023-06-03 22:10:57 +0100

    finished initial layout

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,9 +1,23 @@
 import Header from '@/components/Header/Header'
+import ListItem from '@/components/ListItem/ListItem'
 
 const Home = () => {
   return (
     <div className="h-full w-full overflow-hidden overflow-y-auto rounded-lg bg-neutral-900">
-      <Header>Header</Header>
+      <Header>
+        <div className="mb-2">
+          <h1 className="text-3xl font-semibold text-white">Welcome back</h1>
+          <div className="mt-4 grid grid-cols-1 gap-3 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+            <ListItem image="/images/liked.png" name="Liked Songs" href="" />
+          </div>
+        </div>
+      </Header>
+      <div className="mb-7 mt-2 px-6">
+        <div className="flex items-center justify-between">
+          <h2 className="text-2xl font-semibold text-white">Newest songs</h2>
+        </div>
+        <div>List of Songs!</div>
+      </div>
     </div>
   )
 }
---
+++ src/components/ListItem/ListItem.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { FaPlay } from 'react-icons/fa'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
+interface ListItemProps {
+  image: string
+  name: string
+  href: string
+}
+
+const ListItem = ({ image, name, href }: ListItemProps) => {
+  const router = useRouter()
+
+  const onClick = () => {
+    // add auth before push
+    router.push(href)
+  }
+
+  return (
+    <button
+      type="button"
+      className="group relative flex items-center gap-x-4 overflow-hidden rounded-md bg-neutral-100/10 pr-4 transition hover:bg-neutral-100/20"
+      onClick={onClick}
+    >
+      <div className="relative min-h-[64px] min-w-[64px]">
+        <Image src={image} alt={name} fill />
+      </div>
+      <p className="truncate py-5 font-medium">{name}</p>
+      <div className="absolute right-5 flex items-center justify-center rounded-full bg-green-500 p-4 opacity-0 drop-shadow-md transition hover:scale-110 group-hover:opacity-100">
+        <FaPlay className="text-black" />
+      </div>
+    </button>
+  )
+}
+
+export default ListItem
