da935398cc42e38b1ef3637f5580fe7e0d3ab7e6
RussOakham <73036915+RussOakham@users.noreply.github.com>
2023-07-01 14:23:19 +0100

    Added Liked page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -15,7 +15,11 @@ const Home: React.FC = async () => {
         <div className="mb-2">
           <h1 className="text-3xl font-semibold text-white">Welcome back</h1>
           <div className="mt-4 grid grid-cols-1 gap-3 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-            <ListItem image="/images/liked.png" name="Liked Songs" href="" />
+            <ListItem
+              image="/images/liked.png"
+              name="Liked Songs"
+              href="/liked"
+            />
           </div>
         </div>
       </Header>
